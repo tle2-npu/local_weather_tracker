f51559d60ec8ef54442d82cc09fbc9323ef7715d
tle2-npu <tle2@northpark.edu>
2025-10-03 08:10:14 -0500

    created table for database

--- database.py
+++ database.py
@@ -4,12 +4,14 @@ DB_FILE = "weather.db"
 
 def get_connection():
     conn = sqlite3.connect(DB_FILE)
-    conn.row_factory = sqlite3.Row  # to get dict-like rows
+    conn.row_factory = sqlite3.Row  
     return conn
 
 def create_table():
     conn = get_connection()
     cur = conn.cursor()
+    
+    cur.execute()
     conn.commit()
     cur.close()
     conn.close()
