44f11d4a51d19d2518c132636d8dac2599cf33de
tle2-npu <tle2@northpark.edu>
2025-10-03 08:16:31 -0500

    created class

--- models.py
+++ models.py
@@ -1,2 +1,12 @@
 from pydantic import BaseModel
 from typing import Optional
+
+# Create class
+class Observation(BaseModel):
+    city: str
+    country: str
+    latitude: float
+    longitude: float
+    temperature: float
+    windspeed: float
+    observation_time: str
