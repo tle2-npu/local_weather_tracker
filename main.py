94f21b36062f36871db508323a749a0fd0cd3e8d
tle2-npu <tle2@northpark.edu>
2025-10-18 17:26:59 -0500

    Added API calls for geocoding and current weather retrieval

--- main.py
+++ main.py
@@ -4,4 +4,20 @@ Geocoding_URL = "https://geocoding-api.open-meteo.com/v1/search"
 Weather_URL = "https://api.open-meteo.com/v1/forecast?current_weather=true" 
 
 city = "Chicago"  
-geo_params = {"name": city, "count": 1,} 
+
+geo_params = {
+    "name": city, 
+    "count": 1} 
+
+response = requests.get(Geocoding_URL, params=geo_params) 
+print("Geocoding status:", response.status_code) 
+print("Geocoding URL:", response.url) 
+
+weather_params = {
+    "latitude": latitude,
+    "longitude": longitude,
+    "current_weather": True
+    }
+weather_response = requests.get(Weather_URL, params=weather_params)
+print("Weather Status:", weather_response.status_code)
+print("Weather URL:", weather_response.url)
