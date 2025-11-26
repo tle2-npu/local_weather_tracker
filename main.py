a6ecc6f0318603af59aee1c91d0795a78d29370f
tle2-npu <tle2@northpark.edu>
2025-11-25 21:13:13 -0600

    Added simple_get() wrapper with retry and timeout

--- main.py
+++ main.py
@@ -1,35 +1,54 @@
 import requests
+import time 
 
 Geocoding_URL = "https://geocoding-api.open-meteo.com/v1/search" 
 Weather_URL = "https://api.open-meteo.com/v1/forecast?current_weather=true"
 
+retries = 3 
+
+def simple_get(url, params=None):
+    for attempt in range(retries):              
+        try:
+            return requests.get(url, params=params, timeout=10)
+        except requests.exceptions.RequestException:
+            print("Request failed. Retrying...")
+            time.sleep(1)                 
+    return None                           
+
 def fetch_weather_for_city(city, country):
-    # 1. Fetch geocoding
+    # Get latitude/longitude 
     geo_params = {
         "name": city,
         "count": 1,
         "format": "json"
     }
 
-    geo_response = requests.get(Geocoding_URL, params=geo_params, timeout=10)
-    geo_data = geo_response.json()["results"][0]
+    geo_response = simple_get(Geocoding_URL, params=geo_params)
 
+    if not geo_response:
+        print("Geocoding failed")
+        return None
+
+    geo_data = geo_response.json()["results"][0]
     latitude = geo_data["latitude"]
     longitude = geo_data["longitude"]
-
     print("Geocoding status:", geo_response.status_code) 
     print("Geocoding URL:", geo_response.url) 
 
-    # 2. Fetch weather
+    # Get current weather info 
     weather_params = {
         "latitude": latitude,
         "longitude": longitude,
         "current_weather": True
     }
 
-    weather_response = requests.get(Weather_URL, params=weather_params, timeout=10)
-    weather_data = weather_response.json()["current_weather"]
+    weather_response = simple_get(Weather_URL, params=weather_params)
 
+    if not weather_response:
+        print("Weather API failed")
+        return None
+    
+    weather_data = weather_response.json()["current_weather"]
     print("Weather Status:", weather_response.status_code)
     print("Weather URL:", weather_response.url)
 
@@ -43,8 +62,6 @@ def fetch_weather_for_city(city, country):
         "observation_time": weather_data["time"]
     }
 
-
 if __name__ == "__main__":
     data = fetch_weather_for_city("Chicago", "US")
     print(data)
-
